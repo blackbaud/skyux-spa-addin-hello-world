709cd010b0aaefbebcd7fb730c19d536eeb97687
Adam Hickey <adam.hickey@blackbaud.com>
2019-01-30 12:42:59 -0500

    Fix lint

---
+++ src/app/my-button/my-button.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { AddinClientInitArgs } from '@blackbaud/sky-addin-client';
+import { AddinClient } from '@blackbaud/sky-addin-client';
+
+@Component({
+  selector: 'my-button',
+  templateUrl: './my-button.component.html'
+})
+export class MyButtonComponent implements OnInit {
+
+  private addinClient: AddinClient;
+
+  public ngOnInit() {
+
+    this.addinClient = new AddinClient({
+      callbacks: {
+        init: (args: AddinClientInitArgs) => {
+          args.ready({
+            showUI: true,
+            title: 'Scan check',
+            buttonConfig: { style: 1 }
+          });
+        },
+        buttonClick: () => {
+          this.showSkyUxModal();
+        }
+      }
+    });
+  }
+
+  public showSkyUxModal() {
+    // define context for the modal
+    let context = {
+      firstName: 'John',
+      lastName: 'Doe'
+    };
+
+    this.showModal('https://host.nxt.blackbaud.com/addin-modal-demo/add-customer', context);
+  }
+
+  private showModal(url: string, context: any) {
+    let response = this.addinClient.showModal({
+      url: url,
+      context: context
+    });
+
+    response.modalClosed.then((modalResponse) => {
+      let res = JSON.stringify(modalResponse, undefined, 2);
+      console.log(res);
+    });
+  }
+}
