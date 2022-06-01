26698ce83941e4981080e44ad95d62574e81b194
Val Kuppuswamy <val.kuppuswamy@blackbaud.com>
2022-06-01 08:59:40 -0400

    Migrated to SKY UX 5

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import {
+  RouterModule,
+  Routes
+} from '@angular/router';
+
+import {
+  MyButtonRouteIndexComponent
+} from './my-button/index.component';
+
+import {
+  MyTileRouteIndexComponent
+} from './my-tile/index.component';
+
+import {
+  RootRouteIndexComponent
+} from './index.component';
+
+import {
+  NotFoundComponent
+} from './not-found.component';
+
+const routes: Routes = [
+  { path: '', children: [
+    { path: '', component: RootRouteIndexComponent },
+    { path: 'my-button', component: MyButtonRouteIndexComponent },
+    { path: 'my-tile', component: MyTileRouteIndexComponent }
+  ] },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: []
+})
+export class AppRoutingModule { }
